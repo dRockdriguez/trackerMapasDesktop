cee565822198fcb586f4acf42b3ec94c032d79ed
davidrodriguezonetec <david.rodriguez@grupoonetec.com>
2019-01-24 17:09:18 +0100

    pinta puntos mapa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +10,25 @@ import { Component } from '@angular/core';
 export class AppComponent {
   lat: number = 51.678418;
   lng: number = 7.809007;
+  items: Tax [] = [];
+  init = false;
+
+  constructor(db: AngularFirestore) {
+    db.collection('usuarios').valueChanges().subscribe((data: Tax[]) => {
+      this.items = data;
+
+      if (!this.init) {
+        this.lat = data[0].lat;
+        this.lng = data[0].lng;
+        this.init = true;
+      }
+    });
+  }
+}
+
+interface Tax {
+  nombre: string;
+  clave: string;
+  lat: number;
+  lng: number;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AgmCoreModule } from '@agm/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+
+import { AngularFireModule } from '@angular/fire';
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBGN0zZNotkhspzYde1_eMnN8C9qJdkljg",
+    authDomain: "trackertaxis-ef9b9.firebaseapp.com",
+    databaseURL: "https://trackertaxis-ef9b9.firebaseio.com",
+    projectId: "trackertaxis-ef9b9",
+    storageBucket: "trackertaxis-ef9b9.appspot.com",
+    messagingSenderId: "812889912749"
+  }
+};
 
 @NgModule({
   declarations: [
@@ -12,9 +27,10 @@ import { AgmCoreModule } from '@agm/core';
     BrowserModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyD5GzTdXrMwaqaRv93h-5Js7IiKYUZiItk'
-    })
+    }),
+    AngularFireModule.initializeApp(environment.firebase, 'my-app-name')
   ],
-  providers: [],
+  providers: [AngularFirestore],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
